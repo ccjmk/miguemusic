ad12362def8b9b8c9c7fb77d8847ae70997aedda
ccjmk <iaguastalli@gmail.com>
2026-01-28 19:27:02 +0100

    initial commit with spring initializr files

---
+++ src/main/java/com/example/migue_music/MigueMusicApplication.java
@@ -0,0 +1,13 @@
+package com.example.migue_music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigueMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigueMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/migue_music/MigueMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.migue_music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigueMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
